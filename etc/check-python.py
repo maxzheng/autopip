bfbe87b21bfb0f668ae9ca192be8dbd710dd3f67
Max Zheng <maxzheng.os@gmail.com>
2018-06-16 11:41:26 +0000

    Add --autofix option to fix Python installation issues automatically

--- etc/check-python.py
+++ etc/check-python.py
@@ -7,30 +7,70 @@ import subprocess
 import sys
 
 
-IS_LINUX = platform.system() == 'Linux'
+IS_DEBIAN = platform.system() == 'Linux' and (platform.dist()[0] in ('Ubuntu', 'Debian'))
+IS_OLD_DEBIAN = IS_DEBIAN and int(platform.dist()[1].split('.')[0]) < 18
+IS_MACOS = platform.system() == 'Darwin'
 PY_VERSION = '3.6'
 SUDO = 'sudo ' if os.getuid() else ''
+AUTOFIX = '--autofix' in sys.argv
+
+
+def check_sudo():
+    if not run('which sudo', return_output=True):
+        error('! sudo is not installed.')
+        print('  Please ask an administrator to install it and run this again.')
+        sys.exit(1)
+
+
+def check_apt():
+    os.environ['DEBIAN_FRONTEND'] = 'noninteractive'
+    run(SUDO + 'apt-get install -y apt-utils', return_output=True)
+
+
+def check_curl():
+    if not run('which curl', return_output=True):
+        error('! curl is not installed.')
+        if IS_DEBIAN:
+            raise AutoFixSuggestion('To install, run', SUDO + 'apt-get install -y curl')
+        sys.exit(1)
 
 
 def check_python():
-    print('Checking Python...')
     py3_path = run('which python' + PY_VERSION, return_output=True)
     if not py3_path:
         error('! Python ' + PY_VERSION + ' is not installed.')
+
+        if IS_OLD_DEBIAN:
+            raise AutoFixSuggestion('To install, run',
+                                    (SUDO + 'apt-get update',
+                                     SUDO + 'apt-get install -y software-properties-common',
+                                     SUDO + 'add-apt-repository ppa:deadsnakes/ppa',
+                                     SUDO + 'apt-get update',
+                                     SUDO + 'apt-get install -y python' + PY_VERSION))
+
+        elif IS_DEBIAN:
+            raise AutoFixSuggestion('To install, run',
+                                    (SUDO + 'apt-get update', SUDO + 'apt-get install -y python' + PY_VERSION))
+
+        elif IS_MACOS:
+            raise AutoFixSuggestion('To install, run', 'brew install python')
+
         print('  Please install Python ' + PY_VERSION +
               ' per http://docs.python-guide.org/en/latest/starting/installation/')
         sys.exit(1)
 
 
 def check_pip():
-    print('\nChecking pip...')
-
     if not run('which pip3', return_output=True):
-        error('! pip3 does not seem to be installed.')
-        if IS_LINUX:
-            print('  If your package repo (e.g. apt) has a *-pip package for Python ' + PY_VERSION +
-                  ', then install it from there.')
-            print('  To install in Debian/Ubuntu, run: ' + SUDO + 'apt install python3-pip')
+        error('! pip3 is not installed.')
+        if IS_DEBIAN:
+            raise AutoFixSuggestion('To install, run', SUDO + 'apt-get install -y python3-pip')
+
+        elif IS_MACOS:
+            raise AutoFixSuggestion('To install, run', 'curl -s https://bootstrap.pypa.io/get-pip.py | ' +
+                                    SUDO + 'python' + PY_VERSION)
+        print('  If your package repo has a *-pip package for Python ' + PY_VERSION +
+              ', then installing it from there is recommended.')
         print('  To install directly, run: curl -s https://bootstrap.pypa.io/get-pip.py | ' +
               SUDO + 'python' + PY_VERSION)
         sys.exit(1)
@@ -40,20 +80,18 @@ def check_pip():
     if 'python' + PY_VERSION not in version_full:
         print('  ' + version_full.strip())
         error('! pip3 is pointing to another Python version and not Python ' + PY_VERSION)
-        print('  To re-install for Python ' + PY_VERSION + ', run: curl -s https://bootstrap.pypa.io/get-pip.py | ' +
-              SUDO + 'python' + PY_VERSION)
-        sys.exit(1)
+
+        raise AutoFixSuggestion('To re-install for Python ' + PY_VERSION + ', run',
+                                'curl -s https://bootstrap.pypa.io/get-pip.py | ' + SUDO + 'python' + PY_VERSION)
 
     version_str = version_full.split()[1]
     version = tuple(map(_int_or, version_str.split('.', 2)))
     if version < (9, 0, 3):
         error('! Version is', version_str + ', but should be 9.0.3+')
-        print('  To upgrade, run: ' + SUDO + 'pip3 install pip==9.0.3')
-        sys.exit(1)
+        raise AutoFixSuggestion('To upgrade, run', SUDO + 'pip3 install pip==9.0.3')
 
 
 def check_venv():
-    print('\nChecking venv...')
     test_venv_path = '/tmp/check-python-venv'
 
     try:
@@ -62,9 +100,10 @@ def check_venv():
                 raises=True)
 
         except Exception:
-            error('! Could not create virtual environment. Please make sure *-venv package is installed.')
-            if IS_LINUX:
-                print('  To install in Debian/Ubuntu, run: ' + SUDO + 'apt install python' + PY_VERSION + '-venv')
+            error('! Could not create virtual environment.')
+            if IS_DEBIAN:
+                raise AutoFixSuggestion('To install, run', SUDO + 'apt-get install -y python' + PY_VERSION + '-venv')
+            print(' Please make sure Python venv package is installed.')
             sys.exit(1)
 
     finally:
@@ -72,66 +111,58 @@ def check_venv():
 
 
 def check_setuptools():
-    print('\nChecking setuptools...')
-
     try:
         version_str = run('python' + PY_VERSION + ' -m easy_install --version', return_output=True)
 
     except Exception:
         error('! setuptools is not installed.')
-        print('  To install, run: ' + SUDO + 'pip3 install setuptools')
-        sys.exit(1)
+        raise AutoFixSuggestion('To install, run', SUDO + 'pip3 install setuptools')
 
     version_str = version_str.split()[1]
     version = tuple(map(_int_or, version_str.split('.')))
     if version < (39,):
         error('! Version is', version_str + ', but should be 39+')
-        print('  To upgrade, run: ' + SUDO + 'pip3 install -U setuptools')
-        sys.exit(1)
+        raise AutoFixSuggestion('To upgrade, run', SUDO + 'pip3 install -U setuptools')
 
 
 def check_wheel():
-    print('\nChecking wheel...')
-
     try:
         version_str = run('python' + PY_VERSION + ' -m wheel version ', return_output=True)
 
     except Exception:
         error('! wheel is not installed.')
-        print('  To install, run: ' + SUDO + 'pip3 install wheel')
-        sys.exit(1)
+        raise AutoFixSuggestion('To install, run', SUDO + 'pip3 install wheel')
 
     version_str = version_str.split()[1]
     version = tuple(map(_int_or, version_str.split('.')))
     if version < (0, 31):
         error('! Version is', version_str + ', but should be 0.31+')
-        print('  To upgrade, run: ' + SUDO + 'pip3 install -U wheel')
-        sys.exit(1)
+        raise AutoFixSuggestion('To upgrade, run', SUDO + 'pip3 install -U wheel')
 
 
-def check_devel():
-    print('\nChecking dev...')
-
+def check_python_dev():
     include_path = run('python' + PY_VERSION +
                        ' -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())"',
                        return_output=True)
     if not include_path:
         error('! Failed to get Python include path, so not sure if Python dev package is installed')
-        print('  To install in Debian/Ubuntu, run: ' + SUDO + ' apt install python' + PY_VERSION + '-dev')
+        if IS_DEBIAN:
+            raise AutoFixSuggestion('To install, run', SUDO + ' apt-get install -y python' + PY_VERSION + '-dev')
         sys.exit(1)
 
     python_h = os.path.join(include_path.strip(), 'Python.h')
 
     if not os.path.exists(python_h):
         error('! Python dev package is not installed as', python_h, 'does not exist')
-        print('  To install in Debian/Ubuntu, run: ' + SUDO + 'apt install python' + PY_VERSION + '-dev')
+        if IS_DEBIAN:
+            raise AutoFixSuggestion('To install, run', SUDO + 'apt-get install -y python' + PY_VERSION + '-dev')
         sys.exit(1)
 
 
 def run(cmd, return_output=False, raises=False, **kwargs):
     print('+ ' + str(cmd))
 
-    if '"' in cmd:
+    if '"' in cmd or '|' in cmd:
         kwargs['shell'] = True
     elif isinstance(cmd, str):
         cmd = cmd.split()
@@ -162,7 +193,7 @@ def _int_or(value):
 
 def error(*msg):
     msg = ' '.join(map(str, msg))
-    echo(msg, color='red')
+    echo(msg, color=None if AUTOFIX else 'red')
 
 
 def echo(msg, color=None):
@@ -177,11 +208,56 @@ def echo(msg, color=None):
     print(msg)
 
 
-check_python()
-check_pip()
-check_venv()
-check_setuptools()
-check_wheel()
-check_devel()
+class AutoFixSuggestion(Exception):
+    def __init__(self, instruction, cmd):
+        super(AutoFixSuggestion, self).__init__(instruction)
+        self.cmd = cmd
 
-echo('\nPython is alive and well. Good job!', color='green')
+
+checks = [check_python, check_pip, check_venv, check_setuptools, check_wheel, check_python_dev]
+
+if AUTOFIX:
+    checks.insert(0, check_curl)
+    if IS_DEBIAN:
+        checks.insert(0, check_apt)
+    if SUDO:
+        checks.insert(0, check_sudo)
+
+try:
+    last_fix = None
+
+    for check in checks:
+        print('Checking ' + check.__name__.split('_', 1)[1].replace('_', ' '))
+
+        while True:
+            try:
+                check()
+                break
+
+            except AutoFixSuggestion as e:
+                if AUTOFIX:
+                    if e.cmd == last_fix:
+                        error('! Failed to fix automatically, so you gotta fix it yourself.')
+                        sys.exit(1)
+
+                    else:
+                        cmds = e.cmd if isinstance(e.cmd, tuple) else (e.cmd,)
+                        for cmd in cmds:
+                            run(cmd, return_output=True, raises=True)
+
+                        last_fix = e.cmd
+
+                else:
+                    print('  ' + str(e) + ': ' + ' && '.join(e.cmd) + '\n')
+                    print('# Run the above suggested command(s) manually and then re-run to continue checking,')
+                    print('  or re-run using "python - --autofix" to run all suggested commands automatically.')
+                    sys.exit(1)
+
+        print('')
+
+except Exception as e:
+    if not isinstance(e, AutoFixSuggestion):
+        error('!', str(e))
+    sys.exit(1)
+
+echo('Python is alive and well. Good job!', color='green')
